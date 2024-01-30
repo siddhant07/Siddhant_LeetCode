add98dd0f09218cacca816584641f7f9286eda56
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2024-01-30 18:47:19 -0500

    Time: 62 ms (17.76%), Space: 17.3 MB (71.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,8 +6,6 @@ class Solution:
         for c in s:
             if ord(c) in range(97,123) or ord(c) in range(48,58):
                 buf += c 
-                
-        print(buf)
         
         if buf == ''.join(reversed(buf)):
             return True
