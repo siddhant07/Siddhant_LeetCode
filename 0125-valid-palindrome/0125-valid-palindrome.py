c27d88757cd884f1e379713765a2cc1bb3ca5f2f
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2024-01-30 19:00:00 -0500

    Time: 42 ms (81.56%), Space: 17.1 MB (89.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,11 +2,30 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         buf = ""
         s = s.lower()
+        l = 0
+        r = len(s)-1
         
-        for c in s:
-            if ord(c) in range(97,123) or ord(c) in range(48,58):
-                buf += c 
+        # for c in s:
+        #     if ord(c) in range(97,123) or ord(c) in range(48,58):
+        #         buf += c 
+        # return buf == ''.join(reversed(buf))
         
-        if buf == ''.join(reversed(buf)):
-            return True
+        while(l < r):
+            if s[l] == s[r] and s[l].isalnum() and s[r].isalnum():
+                l += 1
+                r -= 1
+            
+            elif s[l] != s[r] and s[l].isalnum() and s[r].isalnum():
+                return False
+            
+            elif not s[l].isalnum():
+                while(not s[l].isalnum() and l<r):
+                    l += 1
+                    
+            else:
+                while(not s[r].isalnum() and l<r):
+                    r -= 1
+                    
+        return True
+            
         
