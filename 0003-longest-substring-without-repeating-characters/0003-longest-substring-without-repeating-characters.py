dec3ec903b1ac866a027a01d9196e2bf1e322219
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2024-02-06 18:05:34 -0500

    Time: 61 ms (48.17%), Space: 16.7 MB (71.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,17 +7,13 @@ class Solution:
         while r < len(s):
             if s[r] not in newList:
                 newList.add(s[r])
-                print(s[r] + " if")
                 cur += 1
-                print(cur)
             else:
-                print(s[r] + " else")
                 while s[l] != s[r]:
                     newList.remove(s[l])
                     l += 1
                 l += 1
                 maxlen = max(maxlen, cur)
-                print(l, maxlen)
                 cur = r - l + 1
             r += 1
             maxlen = max(maxlen, cur)
