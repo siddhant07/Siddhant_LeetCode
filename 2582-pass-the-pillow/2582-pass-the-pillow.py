325be18c8608b9817778b59fe032de971f9e2544
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2024-07-06 17:28:35 -0400

    Time: 28 ms (91.13%), Space: 16.5 MB (77.09%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,8 +1,6 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         
-        time //(n-1)
-        
         if (time //(n-1))%2 != 0:
             return n - time%(n-1)
         else:
