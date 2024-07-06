44d1f460504f9779bf703f2dbf7b5c28be17a9ad
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2024-07-06 17:28:26 -0400

    Time: 31 ms (77.34%), Space: 16.6 MB (28.82%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,9 +1,9 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         
-        chakkar = time //(n-1)
+        time //(n-1)
         
-        if chakkar%2 != 0:
+        if (time //(n-1))%2 != 0:
             return n - time%(n-1)
         else:
             return time%(n-1) + 1
