d7be57a45c5bdec6fb5bee8c584fe9c9b2dd70bf
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2026-01-28 01:56:49 -0500

    Time: 808 ms (23.06%), Space: 23 MB (5.53%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
         res = []
         nums.sort()
-        
-        for i,a in enumerate(nums):
-            if a == nums[i-1] and i>0:
-                continue
-            
-            else:
-                l=i+1
-                r=len(nums)-1
-                while(l<r):
-                    if(nums[l]+nums[r]+nums[i] == 0):
-                        res.append([nums[i], nums[l], nums[r]])
-                        l += 1
-                        while nums[l] == nums[l-1] and l<r:
-                            l += 1
-                    elif (nums[l] + nums[r] + nums[i] < 0):
-                        l += 1
-                    else:
-                        r -=1
-            
+
+        for i in range(len(nums)):
+            if nums[i]>0:
+                break
+            if i==0 or nums[i-1] != nums[i]:
+                self.twosum(nums, i, res)
         return res
+
+    def twosum(self, nums:List[int], i: int, res:List[List[int]]):
+        seen = set()
+        j = i+1
+
+        while j<len(nums):
+            comp = -nums[i]-nums[j]
+            if comp in seen:
+                res.append([nums[i],nums[j], comp])
+                while j+1 < len(nums) and nums[j] == nums[j+1]:
+                    j+=1
+            seen.add(nums[j])
+            j+=1
+        
+
+            
+
+        
+        
