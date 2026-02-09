deaac7a1200864df417816c5c3de0021892ab4da
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2026-02-09 08:46:33 -0800

    Time: 50 ms (11.26%), Space: 19.3 MB (46.82%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,7 +1,4 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        summer = address.split(".")
-        print (summer)
-
-        return summer[0]+"[.]"+summer[1]+"[.]"+summer[2]+"[.]"+summer[3]
+        return address.replace(".","[.]")
         
