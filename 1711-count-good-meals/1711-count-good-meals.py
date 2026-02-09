587ba89ec902f1160fd6dcf03d34bbf8cbc00c91
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2026-02-09 08:29:49 -0800

    Time: 325 ms (83.42%), Space: 27 MB (59.63%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -1,9 +1,12 @@
 class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
-        ans = 0
         freq = defaultdict(int)
-        for x in deliciousness: 
-            for k in range(22): ans += freq[2**k - x]
-            freq[x] += 1
+        for x in deliciousness: freq[x] += 1
+        
+        ans = 0
+        for x in freq: 
+            for k in range(22): 
+                if 2**k - x <= x and 2**k - x in freq: 
+                    ans += freq[x]*(freq[x]-1)//2 if 2**k - x == x else freq[x]*freq[2**k-x]
         return ans % 1_000_000_007
         
