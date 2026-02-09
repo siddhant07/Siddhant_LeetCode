7f04b0cffaf8a684c53db12d46a38714c54f2627
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2026-02-09 08:54:21 -0800

    Time: 7 ms (52.37%), Space: 23 MB (18.43%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,23 +1,24 @@
 class Solution:
     def updateBoard(self, board, click):
 
-        if board[click[0]][click[1]] == 'M':                # <-- handle case that
-            board[click[0]][click[1]] = 'X' ; return board  #     click is mined
+        if board[click[0]][click[1]] == 'M':                # handle case that
+            board[click[0]][click[1]] = 'X'
+            return board  # click is mined
 
         adjacent = lambda x,y : [(x+dx,y+dy) for dx in range(-1,2) for dy in range(-1,2) 
                 if (dx or dy) and 0 <= x+dx < len(board) and 0 <= y+dy < len(board[0])]
         
         def dfs(x: int,y: int)-> None:                  
             adj = adjacent(x,y)                                                          
-            mines = sum(board[X][Y] == 'M' for X,Y in adj)  # <-- count up adjacent mines 
+            mines = sum(board[X][Y] == 'M' for X,Y in adj)  # count up adjacent mines 
             if  mines:
-                board[x][y] = str(mines)                    # <-- If mines, write count...
+                board[x][y] = str(mines)                    # If mines, write count
             else:    
-                board[x][y] = 'B'                           # <-- ... if not, mark it "reveal"
+                board[x][y] = 'B'                           # if not, mark it "reveal"
                 for X,Y in adj:
-                    if board[X][Y] == 'E':                  # <-- explore each adjacent cell
+                    if board[X][Y] == 'E':                  # explore each adjacent cell
                         dfs(X,Y)                            #     if unexplored
             return                                        
                                                             
-        dfs(*click)                                         # <-- start at click
-        return board                                        # <-- return updated board
+        dfs(*click)                                         # start at click
+        return board                                        # return updated board
