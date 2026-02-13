f0c488105808a81edce92c9d9cfbdd334eab8ff9
Siddhant Godshalwar <65456681+siddhant07@users.noreply.github.com>
2026-02-12 18:09:27 -0800

    Time: 212 ms (76.68%), Space: 27.3 MB (48.64%) - LeetHub

---
+++ 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.py
@@ -0,0 +1,29 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+
+# //We implement a classic BFS but the entries in our queue are future objects instead of primitve values. A pool of at most max_workers threads is used to execute getUrl calls asynchronously. Calling result() on our futures blocks until the task is completed or rejected.
+from concurrent import futures
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        hostname = lambda url: url.split('/')[2]
+        seen = {startUrl}
+    
+        with futures.ThreadPoolExecutor(max_workers=16) as executor:
+            tasks = deque([executor.submit(htmlParser.getUrls, startUrl)])
+            while tasks:
+                for url in tasks.popleft().result():
+                    if url not in seen and hostname(startUrl) == hostname(url):
+                        seen.add(url)
+                        tasks.append(executor.submit(htmlParser.getUrls, url))
+        
+        return list(seen)
+        
